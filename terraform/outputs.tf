10af1301ecaf08aead03a2860fbd3971580ebcaa
Akanksha <ak.tech106@gmail.com>
2025-11-02 02:30:31 +0530

    Update outputs for EKS cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,8 +8,14 @@ output "cluster_name" {
   value       = module.eks.cluster_id
 }
 
-output "kubeconfig" {
-  description = "Kubeconfig file content for the cluster"
-  value       = module.eks.kubeconfig
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
   sensitive   = true
 }
+
+output "cluster_security_group_id" {
+  description = "Security group ID attached to the EKS cluster"
+  value       = module.eks.cluster_security_group_id
+}
+
